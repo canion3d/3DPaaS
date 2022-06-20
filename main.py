8c0ae43a8f57fca7dd0b452f61796d8e5bfa5e81
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-06-20 19:06:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import 3d-renderer as 3dr
+import 3d-converter as 3dc
+from pip._internal import main
+
+st.write('\n'
+         '# 3D-Print-as-a-Service x Canion3D\n'
+         '\n'
+         'The resource management solution for Additive Manufacturing!\n'
+         '\n')
+
+
+
